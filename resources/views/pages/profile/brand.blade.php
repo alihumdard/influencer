fbe19c04ee1e7fe777c0633939f7059cf8f55418
Qaiserali21 <qaiserhero21@gmail.com>
2024-10-22 09:22:41 -0700

    modal error fixed

--- resources/views/pages/profile/brand.blade.php
+++ resources/views/pages/profile/brand.blade.php
@@ -420,6 +420,7 @@
             </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         const sidebar = document.getElementById("sidebar");
         const toggleButton = document.getElementById("toggleButton");
